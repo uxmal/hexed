c717044d894789d62b92c3cc22cdc2a801421375
John Källén <johnkal@gmail.com>
2021-04-21 22:55:14 +0200

    Implementd load command

--- hexed/Hexed.cs
+++ hexed/Hexed.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,8 @@ namespace hexed
 {
     class Hexed
     {
+        private byte[] bytes;
+
         public bool IsDirty { get; set; }
 
         public void Run()
@@ -42,7 +45,34 @@ namespace hexed
 
         private void HandleCommand(string[] cmd)
         {
-            //$TODO: something
+            if (cmd[0] == "l")
+            {
+                Load(cmd);
+            }
+            else
+            {
+                Console.WriteLine("Unknown command '{0}'.", cmd[0]);
+            }
+        }
+
+        // Loading a file.
+        // l <filename>
+        private void Load(string[] cmd)
+        {
+            if (cmd.Length != 2)
+            {
+                Console.WriteLine("l command expects a single filename.");
+                return;
+            }
+            try
+            {
+                this.bytes = File.ReadAllBytes(cmd[1]);
+                Console.WriteLine("{0} bytes loaded", bytes.Length);
+            }catch
+            {
+                Console.WriteLine("Unable to read file '{0}'.", cmd[1]);
+
+            }
         }
 
         private string[] ReadCommand()
