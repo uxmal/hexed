744be473d124a4c76d463c2c215f690f5daee147
John Källén <johnkal@gmail.com>
2021-04-21 22:47:25 +0200

    Add project files.

---
+++ hexed/Hexed.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hexed
+{
+    class Hexed
+    {
+        public bool IsDirty { get; set; }
+
+        public void Run()
+        {
+            IsDirty = false;
+            WriteBanner();
+            var cmd = ReadCommand();
+            while (cmd[0] != "q")
+            {
+                HandleCommand(cmd);
+                cmd = ReadCommand();
+            }
+            if (IsDirty)
+            {
+                if (PromptForSaving())
+                {
+                    Save();
+                }
+            }
+
+        }
+
+        private void Save()
+        {
+            throw new NotImplementedException();
+        }
+
+        private bool PromptForSaving()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void HandleCommand(string[] cmd)
+        {
+            //$TODO: something
+        }
+
+        private string[] ReadCommand()
+        {
+            Console.Write("Hexed> ");
+            var line = Console.ReadLine();
+            if (line == null)
+                return new string[] { "q" };
+            var cmd = line.Split();
+            return cmd;
+        }
+
+        private void WriteBanner()
+        {
+            Console.WriteLine("Hexed v1.0");
+        }
+    }
+}
---
+++ hexed/Program.cs
@@ -0,0 +1,23 @@
+﻿
+using System;
+
+namespace hexed
+{
+    class Program
+    {
+        // Handle different commands
+        // l <filename>
+        // s <filename>
+        // s
+        // d 0100
+        // d 0100 0200
+        // w 0023 'hello'
+        // w 0023 48 46 4c 4c 4f
+        // q
+        static void Main(string[] args)
+        {
+            var hexed = new Hexed();
+            hexed.Run();
+        }
+    }
+}
