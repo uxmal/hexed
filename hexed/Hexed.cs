58cd5b66370274c8aeabca149dc0c31e6b98f1dc
John Källén <johnkal@gmail.com>
2021-04-22 16:26:18 +0200

    Added search capability

--- hexed/Hexed.cs
+++ hexed/Hexed.cs
@@ -32,7 +32,6 @@ namespace hexed
                     Save(new string[0]);
                 }
             }
-
         }
 
         private void Save(string[] cmd)
@@ -75,6 +74,10 @@ namespace hexed
             {
                 Save(cmd);
             }
+            else if (cmd[0] == "f")
+            {
+                Find(cmd);
+            }
             else
             {
                 Console.WriteLine("Unknown command '{0}'.", cmd[0]);
@@ -98,13 +101,64 @@ namespace hexed
             }
         }
 
+        private void Find(string []cmd)
+        {
+            if (cmd.Length < 3)
+            {
+                Console.WriteLine("f command expects at least a position and some byte values");
+            }
+            var findPosition = Convert.ToInt32(cmd[1], 16);
+            var patternBytes = new byte[cmd.Length - 2];
+            for (int i = 2; i < cmd.Length; i = i + 1)
+            {
+                var stringByte = cmd[i];
+                var b = Convert.ToInt32(stringByte, 16);
+                patternBytes[i - 2] = (byte)b;
+            }
+            int foundPos = FindPattern(findPosition, patternBytes);
+            if (foundPos <0)
+            {
+                Console.WriteLine("Failed to find pattern");
+                return;
+            }
+            this.position = foundPos;
+            DumpLines(16);
+            this.position = foundPos;
+        }
+
+        private int FindPattern(int findPosition, byte[] patternBytes)
+        {
+            for (int i = findPosition; i <= bytes.Length - patternBytes.Length; ++i)
+            {
+                bool matched = true;
+                for (int j = 0; j < patternBytes.Length; ++j)
+                {
+                    var patternByte = patternBytes[j];
+                    var textByte = bytes[i + j];
+                    if (patternByte != textByte)
+                    {
+                        matched = false;
+                        break;
+                    }
+                }
+                if (matched)
+                    return i;
+            }
+            return -1;
+        }
+
         private void Dump(string[] cmd)
         {
             if (cmd.Length > 1)
             {
                 position = Convert.ToInt32(cmd[1], 16);
             }
-            for (int row = 0; row < 16 && position < bytes.Length; ++row)
+            DumpLines(16);
+        }
+
+        private void DumpLines(int nLines)
+        {
+            for (int row = 0; row < nLines && position < bytes.Length; ++row)
             {
                 DumpLine();
             }
@@ -159,10 +213,9 @@ namespace hexed
                 this.position = 0;
                 this.filename = cmd[1];
                 Console.WriteLine("{0} bytes loaded", bytes.Length);
-            }catch
+            } catch
             {
                 Console.WriteLine("Unable to read file '{0}'.", cmd[1]);
-
             }
         }
 
